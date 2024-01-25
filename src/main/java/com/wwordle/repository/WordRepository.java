de9e417dac1d43e8d31c56db8cad5edc35438c65
l3002 <tanmaymathpal4545@gmail.com>
2024-01-25 12:19:57 +0530

    fix: refactor related issues

--- src/main/java/com/wwordle/repository/WordRepository.java
+++ src/main/java/com/wwordle/repository/WordRepository.java
@@ -1,8 +1,8 @@
 package com.wwordle.repository;
 
-import com.wwordle.word.Word;
+import com.wwordle.data.word.Word;
 import org.springframework.data.repository.Repository;
 
-public interface WordRepository extends Repository<Word,Long> {
+public interface WordRepository extends Repository<Word, Long> {
 
 }
