6eab47f5f7fa249fd0df270b396662da94ab9f86
l3002 <tanmaymathpal4545@gmail.com>
2025-02-18 18:25:56 +0530

    fix: removed JPA repositories
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/main/java/com/wwordle/repository/WordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wwordle.repository;
-
-import com.wwordle.data.word.Word;
-import org.springframework.data.repository.Repository;
-
-public interface WordRepository extends Repository<Word, Long> {
-
-}
